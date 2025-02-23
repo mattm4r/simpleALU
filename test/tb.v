33cfe00bba5b0a9f36c589e83654f0e05b8f95f6
mattm4r <158484314+mattm4r@users.noreply.github.com>
2025-02-23 17:48:30 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tt_um_alu_mattm4r ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_alu_mattm4r user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
