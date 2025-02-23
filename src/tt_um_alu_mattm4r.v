ca9693b050b15c97962698dbed96d08eb1691374
mattm4r <158484314+mattm4r@users.noreply.github.com>
2025-02-23 16:38:28 +0100

    Add files via upload

--- src/tt_um_alu_mattm4r.v
+++ src/tt_um_alu_mattm4r.v
@@ -13,11 +13,12 @@ module tt_um_alu_mattm4r (
 
     // ALU Inputs
     wire [3:0] a, b;
-    wire [1:0] select;
-    
+    wire [1:0] select;    
+
     assign a = ui_in[3:0];
     assign b = ui_in[7:4];
     assign select = uio_in[1:0];
+    assign uio_in[7:2] = 6'b0;
 
     // ALU Outputs
     reg [3:0] out;
@@ -47,7 +48,7 @@ always @(*) begin
 end
 
     // Assign outputs pins must be assigned. If not used, assign to 0.
-    assign uo_out = {sign, zero, carry, out}; // Remove parity and overflow from the output
+    assign uo_out = {1'b0, sign, zero, carry, out}; // Remove parity and overflow from the output
     assign uio_out = 8'b0;   // All output pins must be assigned. If not used, assign 
     assign uio_oe = 8'b0; // Only enable the relevant outputs
     // List all unused inputs to prevent warnings
